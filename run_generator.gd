6ae13f46b0989ad564c699be3cf52dfdb4e4a1b1
slushie/ampersand <slushiesand@gmail.com>
2026-09-22 01:01:32 -0400

    map generation fixed? beginning player rig
    - testing out map generation again found no strange maps even though i dont remember fixing it so i proceeded
    - re-added marker for chase sequence
    - did uvs and skeleton for player (Lazarus) rig

--- run_generator.gd
+++ run_generator.gd
@@ -7,28 +7,36 @@ extends Node2D
 
 var map : Array
 
-var chase_start : Vector2i
+var chase_start : int
 
 func _ready() -> void:
 	var current = _initialize_map()
-	_initialize_path(current, path_length)
+	if chase:
+		chase_start = _chase_sequence()
+	else:
+		chase_start = -1
+	
+	_initialize_path(current, path_length, chase_start)
 	_print_map()
 
-func _process(delta: float) -> void:
-	pass
+func _process(_delta: float) -> void:
+	if Input.is_action_just_pressed("debug"):
+		map = []
+		var current = _initialize_map()
+		_initialize_path(current, path_length, chase_start)
+		_print_map()
 
 func _initialize_map() -> Vector2i:
 	for x in dimensions.x:
 		map.append([])
 		for y in dimensions.y:
 			map[x].append(0)
+	#determines map length.
 	
 	if start < 0 or start >= dimensions.y:
 		start = randi_range(0, dimensions.y - 1)
 	map[0][start] = "S"
-	
-	if chase:
-		_chase_sequence()
+	#any value other than 0 and 1 have a randomized start. 
 		
 	return Vector2i(0, start)
 
@@ -43,25 +51,24 @@ func _print_map() -> void:
 		map_string += "\n"
 	print(map_string)
 	
-func _chase_sequence() -> void:
-	chase_start.x = (dimensions.x / 2) 
+func _chase_sequence() -> int:
+	chase_start = (dimensions.x / 2) 
 	if (dimensions.x / 2) > 5:
-		chase_start.x += randi_range(-2, 2)
-	print(chase_start.x)
-	chase_start.y = randi_range(0, dimensions.y - 1)
+		chase_start += randi_range(-2, 2)
+	print(chase_start)
+	return chase_start
 	
-	map[chase_start.x][chase_start.y] = "C"
-	
-func _initialize_path(current : Vector2i, length: int) -> bool:
+func _initialize_path(current : Vector2i, length: int, chase_room : int) -> bool:
 	var direction : Vector2i
 	
-	match randi_range(0, 8):
+	match randi_range(0, 6):
 		0:
 			direction = Vector2i.UP
 		1:
 			direction = Vector2i.DOWN
 		_:
 			direction = Vector2i.RIGHT
+	#chooses whether the path will go "up", "down", or "right"
 	
 	if length == 0:
 		if ( current.x + 1 <= dimensions.x - 1):
@@ -71,19 +78,23 @@ func _initialize_path(current : Vector2i, length: int) -> bool:
 		return true
 	elif length == -1:
 		length = dimensions.x
+		#if no set path length, path length is the dimensions
 	
 	for i in 4:
 		if (current.x + direction.x >= 0 and current.x + direction.x < dimensions.x and current.y + direction.y >= 0 and current.y + direction.y < dimensions.y and not map[current.x + direction.x][current.y + direction.y]):
 			current += direction
-			map[current.x][current.y] = length
-			if _initialize_path(current, length - 1):
+			
+			if current.x == chase_room:
+				map[current.x][current.y] = "C"
+			else:
+				map[current.x][current.y] = length
+			
+			if _initialize_path(current, length - 1, chase_room):
 				return true
 			else: 
-				#print("fail")
 				map[current.x][current.y] = 0
 				current -= direction
 		direction = Vector2i.RIGHT
-		#lowk this sometimes doesnt work buttt idgaf rn
-	
+		#resets direction, makes map generate in the "right" direction	
 	return false
 	
