12c8ae02dedf1ace3265bfadc7bcf1f90c3e2808
slushie/ampersand <slushiesand@gmail.com>
2026-09-22 19:22:03 -0400

    finish weightpainting, walk and idle animation
    - finished weightpainting all parts, with minor adjustments to bone placement
    - animated idle and mosty finished walk cycle
    - made a debug room for testing

--- lazarus.gd
+++ lazarus.gd
@@ -1,10 +1,22 @@
-class_name Player extends Node2D
+class_name Player extends CharacterBody2D
 
+const SPEED = 300.0
+const JUMP_VELOCITY = -600.0
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	pass # Replace with function body.
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+func _physics_process(delta: float) -> void:
+	
+	var direction := Input.get_axis("left", "right")
+	
+	if direction:
+		$AnimationPlayer.play("walk")
+		velocity.x = direction * SPEED
+	else:
+		$AnimationPlayer.play("idle")
+		velocity.x = (move_toward(velocity.x, 0, SPEED))
+		
+		
+	if not is_on_floor():
+		velocity += get_gravity() * delta
+		#shouldn't ever happen, but just as a precaution to keep the player grounded.
+		
+	move_and_slide()
